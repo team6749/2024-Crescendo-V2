75931acbd97efb5c8cd00b57fcc0d91c6ce8f404
Team 6749 <ERARobotics6749@gmail.com>
2024-03-20 19:44:07 -0500

    add setter. might not work but i want people to see how to do it

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -53,8 +53,8 @@ public class SwerveModule implements Sendable {
         builder.addDoubleProperty("position", this::getModulePositionM, null);
         builder.addDoubleProperty("rotation", () -> getModuleRotation().getDegrees(), null);
         builder.addDoubleProperty("Max Speed", this::getMaxSpeed, this::setMaxSpeed);
-        builder.addDoubleProperty("Target Velocity", () -> m_targetstate.speedMetersPerSecond, null);
-        builder.addDoubleProperty("Target Rotation", () -> m_targetstate.angle.getDegrees(), null);
+        builder.addDoubleProperty("Target Velocity", () -> m_targetstate.speedMetersPerSecond, (newValue) -> {m_targetstate.speedMetersPerSecond = newValue; });
+        builder.addDoubleProperty("Target Rotation", () -> m_targetstate.angle.getDegrees(), (newValue) -> {m_targetstate.angle = Rotation2d.fromDegrees(newValue); });
 
         SmartDashboard.putData("swerve " + name + " velocity pid", velocityPIDController);
         SmartDashboard.putData("swerve " + name + " angle pid", anglePIDController);
