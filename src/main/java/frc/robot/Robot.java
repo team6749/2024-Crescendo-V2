cf6f9c5f1e424b3477630d80a3332899ea279e66
Reshiria <150201755+Reshiria@users.noreply.github.com>
2025-04-09 16:18:17 -0500

    even morenuke

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -81,7 +81,6 @@ public class Robot extends TimedRobot {
     @Override
     public void autonomousInit() {
         m_robotContainer.swerveDrivebase.setModulesNeutralMode(NeutralModeValue.Brake);
-        m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
         // schedule the autonomous command (example)
         if (m_autonomousCommand != null) {
